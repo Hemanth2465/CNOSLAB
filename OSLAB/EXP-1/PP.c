9bd56e6bfc793f2a6a62c9b06c8371ad254822ad
214g5a0506 <214g5a0506@srit.ac.in>
2022-10-18 13:05:19 +0530

    Second PP.c execution Output.

--- OSLAB/EXP-1/PP.c
+++ OSLAB/EXP-1/PP.c
@@ -1,23 +1,18 @@
-/* Name Of the Candidate: K Hemanth Reddy
-   Roll No: 214g5a0506
-   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
-   Date of Creation: 29-08-2022
-   Date of Execution: 29-08-2022
-*/
 
 #include<stdio.h>
-main()
+void main()
 
 {
-int p[20],bt[20],pri[20], wt[20],tat[20],i, k, n, temp; float wtavg,
-tatavg;
-clrscr();
+int p[20],bt[20],pri[20], wt[20],tat[20],i, k, n, temp; 
+float wtavg,tatavg;
+
 printf("Enter the number of processes --- ");
 scanf("%d",&n);
-for(i=0;i<n;i++){
+for(i=0;i<n;i++)
+{
 p[i] = i;
-printf("Enter the Burst Time & Priority of Process %d --- ",i); scanf("%d
-%d",&bt[i], &pri[i]);
+printf("Enter the Burst Time & Priority of Process %d --- ",i); 
+scanf("%d%d",&bt[i], &pri[i]);
 }
 for(i=0;i<n;i++)
 for(k=i+1;k<n;k++)
@@ -41,11 +36,11 @@ tat[i] = tat[i-1] + bt[i];
 wtavg = wtavg + wt[i];
 tatavg = tatavg + tat[i];
 }
-printf("\nPROCESS\t\tPRIORITY\tBURST TIME\tWAITING TIME\tTURNAROUND
-TIME");
+printf("\nPROCESS\t\tPRIORITY\tBURST TIME\tWAITING TIME\tTURNAROUNDTIME");
 for(i=0;i<n;i++)
 printf("\n%d \t\t %d \t\t %d \t\t %d \t\t %d ",p[i],pri[i],bt[i],wt[i],tat[i]);
 
-printf("\nAverage Waiting Time is --- %f",wtavg/n); printf("\nAverage
-Turnaround Time is --- %f",tatavg/n);
-getch();}
+
+printf("\nAverage Waiting Time is --- %f",wtavg/n); 
+printf("\nAverageTurnaround Time is --- %f",tatavg/n);
+}

3f3c1e0f10d2988652b045074278759e1866dc60
214g5a0506 <214g5a0506@srit.ac.in>
2022-10-18 12:50:58 +0530

    First execution Output.

--- OSLAB/EXP-1/FCFS.c
+++ OSLAB/EXP-1/FCFS.c
@@ -1,16 +1,9 @@
-/* Name Of the Candidate:K.Hemanth Reddy
-   Roll No: 214g5a0506
-   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
-   Date of Creation: 29-08-2022
-   Date of Execution: 29-08-2022
-*/
+
 #include<stdio.h>
-#include<conio.h>
-main()
+void main()
 {
 int bt[20], wt[20], tat[20], i, n;
 float wtavg, tatavg;
-clrscr();
 printf("\nEnter the number of processes -- ");
 scanf("%d", &n);
 for(i=0;i<n;i++)
@@ -33,5 +26,5 @@ for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", i, bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
 printf("\nAverage Turnaround Time -- %f", tatavg/n);
-getch();
+
 }

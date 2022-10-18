53ede1bf10c48ec481af87df19428f46030bc1b6
214g5a0506 <214g5a0506@srit.ac.in>
2022-10-18 13:51:46 +0530

    fourth SJF.c execution Output

--- OSLAB/EXP-1/SJF.c
+++ OSLAB/EXP-1/SJF.c
@@ -1,17 +1,8 @@
-/* Name Of the Candidate: K Hemanth Reddy
-   Roll No: 214g5a0506
-   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
-   Date of Creation: 29-08-2022
-   Date of Execution: 29-08-2022
-*/
-
 #include<stdio.h>
-#include<conio.h>
-main()
+void main()
 {
-int p[20], bt[20], wt[20], tat[20], i, k, n, temp; float wtavg,
-tatavg;
-clrscr();
+int p[20], bt[20], wt[20], tat[20], i, k, n, temp; 
+float wtavg,tatavg;
 printf("\nEnter the number of processes -- ");
 
 scanf("%d", &n);
@@ -44,4 +35,5 @@ printf("\n\t PROCESS \tBURST TIME \t WAITING TIME\t TURNAROUND TIME\n");
 for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", p[i], bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
-printf("\nAverage Turnaround Time -- %f", tatavg/n); getch();}
+printf("\nAverage Turnaround Time -- %f", tatavg/n); 
+}

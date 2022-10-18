24d8a31cab56a8ca2864d7382626349138cd2a3e
214g5a0506 <214g5a0506@srit.ac.in>
2022-10-18 13:43:33 +0530

    third RR.c execution Output

--- OSLAB/EXP-1/RR.c
+++ OSLAB/EXP-1/RR.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
-main()
+void main()
 {
 int i,j,n,bu[10],wa[10],tat[10],t,ct[10],max;
 float awt=0,att=0,temp=0;
-clrscr();
 printf("Enter the no of processes -- ");
 scanf("%d",&n);
 for(i=0;i<n;i++)
@@ -40,6 +39,12 @@ printf("\nThe Average Waiting time is -- %f ",awt/n);
 printf("\n\tPROCESS\t BURST TIME \t WAITING TIME\tTURNAROUND TIME\n");
 for(i=0;i<n;i++)
 printf("\t%d \t %d \t\t %d \t\t %d \n",i+1,ct[i],wa[i],tat[i]);
-getch();}
+}
+
+
+
+
+
+
 
 

db209da2ef768cd58ed3d36cd6b9713dbcf8525b
214g5a0506 <214g5a0506@srit.ac.in>
2022-11-09 15:48:44 +0530

    exp5 Worst.c  will be executed successfully

---
+++ exp5/Wout2.c
@@ -0,0 +1,14 @@
+<pre>Memory Management Scheme - First Fit
+Enter the number of blocks:2
+Enter the number of files:2
+
+Enter the size of the blocks:-
+Block 1:2
+Block 2:2
+Enter the size of the files :-
+File 1:2
+File 2:2
+
+File_no:	File_size :	Block_no:	Block_size:	Fragement
+1		2		1		2		0
+2		2		2		2		0</pre>
---
+++ exp5/Wout3.c
@@ -0,0 +1,13 @@
+<pre>	Memory Management Scheme - First Fit
+Enter the number of blocks:1
+Enter the number of files:2
+
+Enter the size of the blocks:-
+Block 1:1
+Enter the size of the files :-
+File 1:1
+File 2:1
+
+File_no:	File_size :	Block_no:	Block_size:	Fragement
+1		1		1		1		0
+2		1		0		0		0</pre>

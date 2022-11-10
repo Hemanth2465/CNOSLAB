b85d77d6db16b6520d3cfe5f626123439e87cf71
214g5a0506 <214g5a0506@srit.ac.in>
2022-11-10 15:14:18 +0530

    My exp6 LRU was successfully executed

---
+++ exp6/LRU/lruout3.c
@@ -0,0 +1,14 @@
+<pre>
+	2	-1	-1
+	2	3	-1
+	2	3	-1
+	2	3	1
+	2	5	1
+	2	5	1
+	2	5	4
+	2	5	4
+	3	5	4
+	3	5	2
+	3	5	2
+	3	5	2
+ no of page faults :7<font color="#4E9A06"><b>srit@srit-HP-Pro-3090-MT</b></font></pre>

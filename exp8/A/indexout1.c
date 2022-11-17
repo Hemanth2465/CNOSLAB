19d3478538b0caccb9c46425895f8e4056a2379a
214g5a0506 <214g5a0506@srit.ac.in>
2022-11-17 15:04:22 +0530

    MY exp 8 index is executed

---
+++ exp8/A/indexout1.c
@@ -0,0 +1,10 @@
+<pre>enter index block	2
+enter no of files on index	4
+hhf sdghas hsda
+
+ allocated
+ file indexed
+ 2-&gt;0:1
+ 2-&gt;0:1
+ 2-&gt;0:1
+ 2-&gt;0:1 Enter 1 to enter more files and 0 to exit</pre>
---
+++ exp8/B/indexout1.c
@@ -0,0 +1,5 @@
+<pre>enter index block	5
+enter no of files on index	aa bb cc dd
+
+ allocated
+ file indexed Enter 1 to enter more files and 0 to exit</pre>
